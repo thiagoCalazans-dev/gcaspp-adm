9ad1e64760ec3cb9c2c9e71c2ecd3ea361295d1c
Thiago Calazans <devt.calazans@gmail.com>
2023-01-08 18:31:12 -0300

    setup tailwild

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>    
+        <meta name="viewport" content="width=device-width, initial-scale=1" />      
+      </Head>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
