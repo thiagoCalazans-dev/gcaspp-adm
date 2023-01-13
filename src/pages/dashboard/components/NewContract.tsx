1563a6f8fdfd32474a003e6fb8bca3f6b6a7ca12
Thiago Calazans <devt.calazans@gmail.com>
2023-01-13 15:30:36 -0300

    newcontract

--- src/pages/dashboard/components/NewContract.tsx
+++ src/pages/dashboard/components/NewContract.tsx
@@ -13,11 +13,11 @@ const contractSchema = z.object({
     .string()
     .min(3, { message: "O usuário precisa ter pelo menos 3 letras." })
     .transform((name) => name.toLowerCase()),
-  // number: z.number().min(1, { message: "o numero deve ser maior que 0" }),
-  // modalityID: z.number(),
-  // initialDate: z.date(),
-  // due_date: z.date(),
-  // first_invoice_date: z.date(),
+  number: z.string().min(1, { message: "o numero deve ser maior que 0" }),
+  modalityId: z.string(),
+  initialDate: z.string(),
+  dueDate: z.string(),
+  firstInvoiceDate: z.string(),
 });
 
 type ContractFormData = z.infer<typeof contractSchema>;
@@ -26,7 +26,7 @@ export function NewContract() {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
+    formState: { isSubmitting, errors },
   } = useForm<ContractFormData>({
     resolver: zodResolver(contractSchema),
   });
@@ -50,6 +50,61 @@ export function NewContract() {
                 id="name"
                 placeholder=""
                 {...register("name")}
+              />
+            </TextInput.Root>
+          </label>
+          <label htmlFor="number" className="flex flex-col">
+            <Text size="sm">Número :</Text>
+            <TextInput.Root>
+              <input
+                type="text"
+                id="number"
+                placeholder=""
+                {...register("number")}
+              />
+            </TextInput.Root>
+          </label>
+          <label htmlFor="modalityId" className="flex flex-col">
+            <Text size="sm">Modalidade :</Text>
+            <TextInput.Root>
+              <input
+                type="text"
+                id="modalityId"
+                placeholder=""
+                {...register("modalityId")}
+              />
+            </TextInput.Root>
+          </label>
+          <label htmlFor="initialDate" className="flex flex-col">
+            <Text size="sm">Data Inicial :</Text>
+            <TextInput.Root>
+              <input
+                type="text"
+                id="initialDate"
+                placeholder=""
+                {...register("initialDate")}
+              />
+            </TextInput.Root>
+          </label>
+          <label htmlFor="dueDate" className="flex flex-col">
+            <Text size="sm">Vencimento :</Text>
+            <TextInput.Root>
+              <input
+                type="text"
+                id="dueDate"
+                placeholder=""
+                {...register("dueDate")}
+              />
+            </TextInput.Root>
+          </label>
+          <label htmlFor="firstInvoiceDate" className="flex flex-col">
+            <Text size="sm">Primeira Fatura :</Text>
+            <TextInput.Root>
+              <input
+                type="text"
+                id="firstInvoiceDate"
+                placeholder=""
+                {...register("firstInvoiceDate")}
               />
             </TextInput.Root>
           </label>
